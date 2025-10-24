519ac677f7cfe2a761eec12508304d3816cb1c04
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-10-24 18:30:46 +0600

    day 3, done

---
+++ Python Operator/main.py
@@ -0,0 +1,20 @@
+# Arithmetic Operator: +, -, *, /, %, **, //
+a = 100
+b = 20
+# print("Addition", a + b)
+# print("Subtraction", a - b)
+# print("Multiplication", a * b)
+# print("Division", a / b)
+# print("Modulus", a % b)
+# print("Exponentiation", a ** b)
+sum1 = 15
+sum2 = 2
+# print("Floor Division", sum1 // sum2)
+
+# Assignment Operator
+d = 5
+d += 5 # 5 + 5 = 10
+d -= 5 # 10 - 5 = 5
+d *= 5 # 5 * 5 = 25
+d /= 5 # 25 / 5 = 5
+print(d)
