4a15db25a9be6c05d9cb263311866d1924229a9e
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-10-25 19:05:06 +0600

    change in list, add in list, remove from list

--- Python Swaping/main.py
+++ Python Swaping/main.py
@@ -16,3 +16,18 @@ newRoll = str(roll)
 # print(newRoll)
 
 # List data type
+fruits = ["apple", "banana", "lemon"]
+fruits[2] = "mango"
+
+# add list item
+fruits.append("lemon") # add to the last in the list
+fruits.insert(1, "cherry") # add to the given index number location, but didn't remove the previous index
+print(fruits)
+
+# remove list item
+li = [1, 2, 3, 4, 5, 6]
+li.remove(2) # write the specific item to remove
+li.pop(3) # remove items through index number and if I didn't write any index number it will remove the last item
+del li[0] # also remove through index number and if I didn't write any index number it will remove/delete the entire list
+li.clear() # remove all item from the list and it will be empty list []
+print(li)
