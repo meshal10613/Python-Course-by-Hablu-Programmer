d8fadf042ba96bdddf05e2a45640818fdcec5dd8
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-10-25 18:31:15 +0600

    user input & type casting done

--- Python Swaping/main.py
+++ Python Swaping/main.py
@@ -5,3 +5,14 @@ a, b = b, a # swap a to b and b to a
 # print(a, b)
 
 # user input
+# username = input("Enter your username: ")
+# password = input("Enter your password: ")
+# # print("My name is", username)
+# print(password)
+
+# type casting - str(), float(), int()
+roll = 10
+newRoll = str(roll)
+# print(newRoll)
+
+# List data type
