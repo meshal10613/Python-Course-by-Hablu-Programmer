d7718e85d836a521cb691515f317a2c7463663c3
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-10-29 18:35:33 +0600

    access tuple done

--- Python Matrix & Tuple/main.py
+++ Python Matrix & Tuple/main.py
@@ -5,4 +5,11 @@ li = [
     15
 ]
 
-print(li[0][1])
+# print(li[0][1])
+
+# Tuples-----------------------
+myTupple = ("Apple", "Banana", "Cherry", "Jackfruit")
+apple = myTupple[0] #access tuple with index
+jackfruit = myTupple[-1] #access the last item of the tuple with -index
+rangeAccess = myTupple[1: 3] # that means index 1 to index 3 - 1 = 2
+print(rangeAccess)
